eab64a90e177241944ae11dfe2b35c705f2e034a
Steve Kaser <stevekaser@protonmail.com>
2026-08-06 19:35:36 -0400

    assets

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include <raylib.h>
+#include <raymath.h>
+
+#define NOB_IMPLEMENTATION
+#include "./nob.h"
+
+#define ASSETS_DIR "./assets"
+#define MOXICA_CHESS_ICONS_DIR ASSETS_DIR "/moxica_chess_set/Icons"
+#define MOXICA_CHESS_PIECES_DIR ASSETS_DIR "/moxica_chess_set/Pieces"
 
 int main(void)
 {
-    InitWindow(800, 600, "Raylib Template");
+    InitWindow(800, 480, "Chess Score Keeper");
     SetTargetFPS(60);
+
+    Texture2D whiteKing = LoadTexture(MOXICA_CHESS_ICONS_DIR"/White_king.png");
+    Texture2D blackKing = LoadTexture(MOXICA_CHESS_ICONS_DIR"/Black_king.png");
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(GetColor(0x181818FF));
+        DrawTextureEx(whiteKing, CLITERAL(Vector2){10, 10}, 0, 0.25f, WHITE);
+        DrawTextureEx(blackKing, CLITERAL(Vector2){100, 10}, 0, 0.25f, WHITE);
         EndDrawing();
     }
+
+    UnloadTexture(whiteKing);
+    UnloadTexture(blackKing);
     CloseWindow();
     return 0;
 }
